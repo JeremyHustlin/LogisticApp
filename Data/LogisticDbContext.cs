1b8f368c17309456e54f1acc30179acbf3706c81
Mihai <Dell@DESKTOP-EI19762>
2022-08-14 01:27:41 +0300

    migration

--- Data/LogisticDbContext.cs
+++ Data/LogisticDbContext.cs
@@ -6,15 +6,16 @@ using LogisticApp.ModelsDelivers;
 using LogisticApp.ModelsForWarder;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
+
 namespace LogisticApp.Data
 {
-    public class LogisticDbContext: IdentityDbContext<Driver>
+    public class LogisticDbContext : IdentityDbContext<Driver>
 
     {
-            public LogisticDbContext(DbContextOptions<LogisticDbContext> options) : base(options)
-            {
-                //
-            }
+        public LogisticDbContext(DbContextOptions<LogisticDbContext> options) : base(options)
+        {
+            //
+        }
 
 
 
@@ -30,6 +31,7 @@ namespace LogisticApp.Data
         public DbSet<Schedule> Schedules { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<VehicleLocation> VehiclesLocations { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Customer>()
@@ -43,8 +45,9 @@ namespace LogisticApp.Data
         }
 
 
-    }
 
+    }
+}
 
 
     /* protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -63,19 +66,4 @@ namespace LogisticApp.Data
              .WithMany(c => c.Id)
              .IsRequired()
              .OnDelete(DeleteBehavior.Cascade);
-     }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Customer>()
-        .HasOne<Order>(e => e.Orders)
-        .WithMany(d => d.Customers)
-        .HasForeignKey(e => e.OrderId)
-        .IsRequired(true)
-        .OnDelete(DeleteBehavior.Cascade);
-
-        base.OnModelCreating(modelBuilder); */
-    
-}
-
-
-
+    */
